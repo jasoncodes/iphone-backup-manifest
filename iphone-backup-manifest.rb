5a68691b7232f51759bb59d14b26349fc7fcd444
Jason Weathered <jason@jasoncodes.com>
2011-07-23 21:08:17 +1000

    Use Bundler.

--- iphone-backup-manifest.rb
+++ iphone-backup-manifest.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 
-$:.unshift File.dirname(__FILE__) + '/lib'
+require 'pathname'
+ENV['BUNDLE_GEMFILE'] ||= (Pathname.new(__FILE__).realpath.dirname + 'Gemfile').to_s
+$:.unshift (Pathname.new(__FILE__).realpath.dirname + 'lib').to_s
+
 require 'rubygems'
+require 'bundler'
+Bundler.require
+
+$:.unshift File.dirname(__FILE__) + '/lib'
 require 'iphone_backup_manifest'
 
 manifest = read_iphone_backup_manifest
